7a41a0b9e5e4ba4a2c030eb0262ae467c303388e
Worley Sacramento <78088577+WorleySacramento@users.noreply.github.com>
2023-11-08 11:27:15 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images:{unoptimized : true}
   
 }
-module.exports = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'assets.example.com',
-        port: '',
-        pathname: '/account123/**',
-      },
-    ],
-  },
-}
+// module.exports = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'assets.example.com',
+//         port: '',
+//         pathname: '/account123/**',
+//       },
+//     ],
+//   },
+// }
 
 module.exports = nextConfig
